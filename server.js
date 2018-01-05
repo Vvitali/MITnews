194f31cd70a0fd3376d403b76d28d323740d105b
Vitali Vorobyev <vvitali26@gmail.com>
2018-01-05 11:44:45 -0500

    basic scraping

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var handlebars = require("express-handlebars");
 var mongoose = require('mongoose');
 var bodyParser = require("body-parser");
 var cheerio = require("cheerio");
+
 var request = require("request");
 
 var app = express();
@@ -11,5 +12,12 @@ app.use(bodyParser.urlencoded({ extended: false}));
 
 request("http://news.mit.edu", (error, response, body)=>{
 	if(error) console.error(error);
-
+	var $ = cheerio.load(body);
+	var news = $("div.descr >h3");
+	console.log(news[0].children[0].data);
+	for(var newsItem in news){
+		console.log(news[newsItem].children[0].data);
+	}
 })
+	//#latest-news-area > ul > li.article > div > a > div > 
+	
