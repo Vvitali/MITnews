97b1d03542cfc154e42f2f1a2641ca0310ff3ad9
Vitali Vorobyev <vvitali26@gmail.com>
2018-01-10 13:46:08 -0500

    mobile view fixed (it fired resize anytime when you scroll the page)

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,21 +1,25 @@
 
 let displayCanvas = ()=>{
+	if($(window).width() != width && $(window).height() != height){
 
-	var canvas = document.getElementById('backgroundC');
-	var ctx = canvas.getContext('2d');
-	var width = window.innerWidth;
-	var height = window.innerHeight; 
-	console.log(window.innerWidth+":"+window.innerHeight);
-	canvas.width  = width;
-	canvas.height = height;
+		var canvas = document.getElementById('backgroundC');
+		var ctx = canvas.getContext('2d');
+		var width = window.innerWidth;
+		var height = window.innerHeight; 
+		console.log(window.innerWidth+":"+window.innerHeight);
+		canvas.width  = width;
+		canvas.height = height;
 
-	var background = new Image();
-	background.src = "http://lorempixel.com/"+width+"/"+height+"/abstract";
+		var background = new Image();
+		background.src = "https://lorempixel.com/"+width+"/"+height+"/abstract";
 
-	// Make sure the image is loaded first otherwise nothing will draw.
-	background.onload = function(){
-		ctx.clearRect(0, 0, width, height);
-		ctx.drawImage(background,0,0);   
+		// Make sure the image is loaded first otherwise nothing will draw.
+		background.onload = function(){
+			ctx.clearRect(0, 0, width, height);
+			ctx.drawImage(background,0,0);   
+		}
+
 	}
 }
+
 displayCanvas();
