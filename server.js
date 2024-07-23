7df306a299f4dd7a8fc3a676a5c39a520787f8ea
Vitaliy Vorobyev <vitaliy.vorobyev.job@gmail.com>
2024-07-23 11:04:40 -0400

    refactor(server): update server with new deps

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 var express = require("express");
 
-var handlebars = require("express-handlebars");
+var { engine } = require("express-handlebars");
 
 var bodyParser = require("body-parser");
 var cheerio = require("cheerio");
 var Table = require('cli-table');
-
-var request = require("request");
 
 var controllers =require("./controllers/index.js");
 
@@ -14,8 +12,8 @@ var app = express();
 var PORT =  process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: false}));
 
-app.engine('handlebars', handlebars({defaultLayout: 'layout'}));
-app.set('view engine', 'handlebars');
+app.engine('handlebars', engine({defaultLayout: 'layout'}));
+app.set('view engine', engine);
 app.use(express.static('public'))
 
 var table = new Table({
@@ -23,54 +21,50 @@ var table = new Table({
 });
 
 //this function renders a list of news from MIT
-app.get("/", function(req, res){
+app.get("/", async function (req, res) {
 	console.log("/");
 	var list = [];
 
-	request("http://news.mit.edu", (error, response, body)=>{
-		if(error) console.error(error);
-		var $ = cheerio.load(body);
+	try {
+		const response = await axios.get("http://news.mit.edu");
+		var $ = cheerio.load(response.data);
 		var news = $("li>div", "#latest-news-area");
 		var mainTopics = $("li>div", "#featured-items");
 
-		//href : news[i].attribs.href
-		//img src : news[i].children[1].children[0].attribs.src
-		//title : 	   news[i].children[1].children[1].children[0].children[0].data
-		//desrciption: news[i].children[1].children[1].children[1].children[0].data
-		var len = news.length-1;
-		console.log("Number:"+len)
-		for(var i = len; i>= 0; i--){
-			console.log(i+": "+$(news[i]).find("h3").text());
+		var len = news.length - 1;
+		console.log("Number:" + len);
+		for (var i = len; i >= 0; i--) {
+			console.log(i + ": " + $(news[i]).find("h3").text());
 			list.push({
-				title: $(news[i]).find("h3").text()
-				, body:$(news[i]).find("p").text()
-				, photoUrl: $(news[i]).find("img").attr('src')
-				, url: $(news[i]).find("a").attr('href')
+				title: $(news[i]).find("h3").text(),
+				body: $(news[i]).find("p").text(),
+				photoUrl: $(news[i]).find("img").attr('src'),
+				url: $(news[i]).find("a").attr('href')
 			});
 			list.push({
-				title: $(mainTopics[i]).find("h3").text()
-				, body:$(mainTopics[i]).find("p").text()
-				, photoUrl: $(mainTopics[i]).find("img").attr('src')
-				, url: $(mainTopics[i]).find("a").attr('href')
+				title: $(mainTopics[i]).find("h3").text(),
+				body: $(mainTopics[i]).find("p").text(),
+				photoUrl: $(mainTopics[i]).find("img").attr('src'),
+				url: $(mainTopics[i]).find("a").attr('href')
 			});
-
-
-			//table.push([news[i].children[0].children[0].data, news[i].children[1].children[0].data.substring(0, 80)]);
 		}
-		controllers.Articles.create(list, function(err, done){
-			if(err){
-				console.log("Error:")
+
+		controllers.Articles.create(list, function (err, done) {
+			if (err) {
+				console.log("Error:");
 				console.log(err.errors.title.message);
-			} 
-			controllers.getLastArticles(50, (data)=>{
-				res.render("index", {news: data});
+			}
+			controllers.getLastArticles(50, (data) => {
+				res.render("index", { news: data });
 			});
-		})
+		});
 		var temp = {};
 		temp["news"] = list;
-		
-	});
+	} catch (error) {
+		console.error(error);
+	}
 });
+
 
 app.post("/addnote", function(req, res){
 	var articleId = req.body.id;
